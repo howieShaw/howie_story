c78c81da0c20d0432301b54408df1f631628e9af
xiaohaoyun <xiaohaoyun@meituan.com>
2018-05-31 19:08:57 +0800

    添加单链表对象

--- howie-story/story-biz/src/main/java/com/howie/story/biz/process/SLinkList.java
+++ howie-story/story-biz/src/main/java/com/howie/story/biz/process/SLinkList.java
@@ -2,6 +2,8 @@ package com.howie.story.biz.process;
 
 import com.howie.story.biz.bean.SNode;
 
+import javax.net.ssl.SNIHostName;
+
 /**
  * @Author:xiaohaoyun
  * @Description： 单项链表
@@ -11,11 +13,83 @@ import com.howie.story.biz.bean.SNode;
 public class SLinkList<T> {
     public SNode<T> top;
     public SNode<T> current;
+    public int size;
 
     public SLinkList (SNode<T> top) {
         this.top = top;
         this.current = this.top;
+        size = 1;
     }
+
+    public void add (SNode<T> node) {
+        if (node == null) {
+            System.out.println("~~添加node 信息为空");
+            return;
+        }
+        current.next = node;
+        current = current.next;
+        size++;
+    }
+
+    public SNode<T> getSNode (T data) {
+        if (data == null|| size ==0) {
+            return null;
+        }
+        SNode temp = top;
+        int index = 0;
+        while (index < size) {
+            if (temp.data.equals(data)) {
+                return temp;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        return null;
+    }
+
+    public SNode<T> getSNodeByIndex (int index) {
+        if (size == 0) {
+            return null;
+        }
+        if (index >= size) {
+            System.out.println("超出链表长度");
+            return null;
+        }
+
+        SNode temp = top;
+        int tempIndex = 0;
+        while (tempIndex <= index) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public SNode<T> delete (T data) {
+       if (data == null) {
+           System.out.println("传入空");
+       }
+       SNode temp = top;
+       int index = 0;
+       SNode result = null;
+       while (index < size) {
+           SNode pre = temp;
+           if (temp == null) {
+               return null;
+           }
+           if (!temp.data.equals(data)) {
+               temp = temp.next;
+               continue;
+           }
+           result = temp;
+           pre.next = temp.next;
+           temp = null;
+           size--;
+       }
+
+        return result;
+    }
+
 
 
 }
